f57eb821bbf38296c17cc6e3119953d6a5f78973
MRAO0212 <mayureshvrao@gmail.com>
2020-08-14 16:56:52 -0700

    create project

---
+++ Storm-Viewer/Storm-Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storm-Viewer
+//
+//  Created by Mayuresh Rao on 8/14/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
